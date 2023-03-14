01cb0761f35ee2c7627b812636e1adaee5fa4f40
JosepUPC <125818818+JosepUPC@users.noreply.github.com>
2023-03-14 17:08:03 +0100

    Update genRndSignal.m

--- genRndSignal.m
+++ genRndSignal.m
@@ -10,11 +10,11 @@ in=0;
 x=0;
 n=[0:N-1];
 A_makedist=makedist("Uniform","lower",0,"upper",1);
-A_sub_K=random(A_makedist,1);
+A_sub_K=rand(A_makedist,1);
 F_makedist=makedist("Uniform","lower",0,"upper",B);
-F_sub_K=random(F_makedist,1);
+F_sub_K=rand(F_makedist,1);
 Omega_makedist=makedist("Uniform","lower",0,"upper",2*pi);
-Omega_sub_K=random(Omega_makedist,1);
+Omega_sub_K=rand(Omega_makedist,1);
 
 for in=1:K
     x=x+(A_sub_K*cos(2*pi*n*F_sub_K+Omega_sub_K));
