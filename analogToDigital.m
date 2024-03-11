7fc29c505f81ae404f9eb11e7714fe9f5f0c8b02
JosepUPC <github.jeb@gmail.com>
2024-03-11 15:16:33 +0100

    Update

--- analogToDigital.m
+++ analogToDigital.m
@@ -1,16 +1,32 @@
-function [b] = analogToDigital(x)
+%%  FUNCTION STATEMENT
+%
+%
+%
+%%
+function [ b ] = analogToDigital( x )
+
     partition = linspace(-2, 2, 15);
-    codebook = [0 0 0 0 ; 0 0 0 1 ; 0 0 1 0 ; 0 0 1 1 ; ...
-    0 1 0 0 ; 0 1 0 1 ; 0 1 1 0 ; 0 1 1 1 ; ...
-    1 0 0 0 ; 1 0 0 1 ; 1 0 1 0 ; 1 0 1 1 ; ...
-    1 1 0 0 ; 1 1 0 1 ; 1 1 1 0 ; 1 1 1 1 ];
-    b = zeros(4*length(x), 1);
-    for i = 1:length(x)
-        [a, ind] = find((x(i) - partition) < 0, 1);
-        if isempty(ind) 
-            b((1:4)+4*(i-1)) = codebook(16, :);
-        else 
-            b((1:4)+4*(i-1)) = codebook(ind, :);
+    codebook  = (0:length(partition));
+    xq = quantize(x, partition, codebook);
+    bq = dec2bin(xq, 4);
+
+    b = zeros(4*length(bq), 1);
+    for i = 1 : length(bq)
+        for j = 1 : 4
+            b(4*(i-1)+j) = str2double( bq(i, j) );
         end
     end
+    
+end
+
+function [ iq ] = quantize( x, partition, codebook )
+
+    iq = zeros(size(x));
+    for i = 1 : length(x)
+        [~, ind] = find((x(i) - partition) < 0, 1);
+        if isempty(ind), iq(i) = codebook(end);
+        else           , iq(i) = codebook(ind);
+        end
+    end
+
 end
